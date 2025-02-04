d1e7aa74383bb40def0f4da7d7e35adfe438de92
Hans de Goede <hdegoede@redhat.com>
2025-02-04 17:34:54 +0100

    psys: Use cdev_device_add() for /dev/ipu-psys0
    Use cdev_device_add() for /dev/ipu-psys0 so that its kobj parents gets
    set properly by calling cdev_set_parent().
    
    "cdev_set_parent() sets a parent kobject which will be referenced
     appropriately so the parent is not freed before the cdev. This
     should be called before cdev_add."
    
    Signed-off-by: Hans de Goede <hdegoede@redhat.com>

--- drivers/media/pci/intel/ipu6/psys/ipu-psys.c
+++ drivers/media/pci/intel/ipu6/psys/ipu-psys.c
@@ -1328,17 +1328,6 @@ static int ipu6_psys_probe(struct auxiliary_device *auxdev,
 
 	psys->power_gating = 0;
 
-	cdev_init(&psys->cdev, &ipu_psys_fops);
-	psys->cdev.owner = ipu_psys_fops.owner;
-
-	rval = cdev_add(&psys->cdev, MKDEV(MAJOR(ipu_psys_dev_t), minor), 1);
-	if (rval) {
-		dev_err(dev, "cdev_add failed (%d)\n", rval);
-		goto out_unlock;
-	}
-
-	set_bit(minor, ipu_psys_devices);
-
 	spin_lock_init(&psys->ready_lock);
 	spin_lock_init(&psys->pgs_lock);
 	psys->ready = 0;
@@ -1414,11 +1403,18 @@ static int ipu6_psys_probe(struct auxiliary_device *auxdev,
 	psys->dev.devt = MKDEV(MAJOR(ipu_psys_dev_t), minor);
 	psys->dev.release = ipu_psys_dev_release;
 	dev_set_name(&psys->dev, "ipu-psys%d", minor);
-	rval = device_register(&psys->dev);
+	device_initialize(&psys->dev);
+
+	cdev_init(&psys->cdev, &ipu_psys_fops);
+	psys->cdev.owner = ipu_psys_fops.owner;
+
+	rval = cdev_device_add(&psys->cdev, &psys->dev);
 	if (rval < 0) {
 		dev_err(dev, "psys device_register failed\n");
 		goto out_release_fw_com;
 	}
+
+	set_bit(minor, ipu_psys_devices);
 
 	/* Add the hw stepping information to caps */
 	strscpy(psys->caps.dev_model, IPU6_MEDIA_DEV_MODEL_NAME,
@@ -1443,7 +1439,6 @@ out_free_pgs:
 	ipu_psys_resource_pool_cleanup(&psys->resource_pool_running);
 out_mutex_destroy:
 	mutex_destroy(&psys->mutex);
-	cdev_del(&psys->cdev);
 	if (psys->sched_cmd_thread) {
 		kthread_stop(psys->sched_cmd_thread);
 		psys->sched_cmd_thread = NULL;
@@ -1481,10 +1476,9 @@ static void ipu6_psys_remove(struct auxiliary_device *auxdev)
 
 	ipu_psys_resource_pool_cleanup(&psys->resource_pool_running);
 
-	device_unregister(&psys->dev);
+	cdev_device_del(&psys->cdev, &psys->dev);
 
 	clear_bit(MINOR(psys->cdev.dev), ipu_psys_devices);
-	cdev_del(&psys->cdev);
 
 	mutex_unlock(&ipu_psys_mutex);
 
